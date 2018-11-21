b5f21bf62b4fd80b4ca0d98899f76286d9f23a10
Max Base <MaxBaseCode@Gmail.Com>
2018-11-21 06:39:53 +0330

    Update RunBenchmark.js

--- RunBenchmark.js
+++ RunBenchmark.js
@@ -1,11 +1,11 @@
 /**
 *
-* @Name : TinyTimer.js
+* @Name : RunBenchmark.js
 * @Version : 1.0
 * @Programmer : Max
 * @Date : 2018-11-21
-* @Released under : https://github.com/BaseMax/TinyTimerJs/blob/master/LICENSE
-* @Repository : https://github.com/BaseMax/TinyTimerJs
+* @Released under : https://github.com/BaseMax/RunBenchmarkJs/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/RunBenchmarkJs
 *
 **/
 ;(function(window,document)
