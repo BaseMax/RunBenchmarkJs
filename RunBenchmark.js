9aabfde443c6a198a171a608ded4339b7310eb2f
Max Base <MaxBaseCode@Gmail.Com>
2018-11-21 05:54:53 +0330

    Create RunBenchmark.js

---
+++ RunBenchmark.js
@@ -0,0 +1,10 @@
+/**
+*
+* @Name : RunBenchmark.js
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2018-11-21
+* @Released under : https://github.com/BaseMax/RunBenchmarkJs/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/RunBenchmarkJs
+*
+**/
